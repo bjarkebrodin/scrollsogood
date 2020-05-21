c92fa23dc6ae09644460cf0607177ae9c7cb9812
bjal <bjal@itu.dk>
2020-05-21 17:19:35 +0200

    add keys, touch and out of bounds error

--- ssg.js
+++ ssg.js
@@ -25,7 +25,10 @@ const ssg = function()
         };
     }
 
-    const TIMEOUT = 700;
+    const TIMEOUT = 800;
+    const TOUCH_SENSITIVITY = 5;
+    const UP_KEYS = [37, 38];
+    const DOWN_KEYS = [39, 40];
 
     let current = 0;
     let max = 0;
@@ -33,7 +36,15 @@ const ssg = function()
 
     // Private Methods
     const setPage = (pageNum) => {
+        if (pageNum > max || pageNum < 0) {
+            console.error(`ssg error: page number ${pageNum} out of bounds for [0;${max}]`);
+        } 
+
+        document.body.style.transition = 'none';
         document.body.style.transform = `translateY(-${unit.vh() * 100 * pageNum}px)`;
+        document.body.style.transition = 'all .5s linear';
+        
+        current = pageNum;
     }
  
     // Public Methods
@@ -42,9 +53,9 @@ const ssg = function()
     const getTransitionDuration = () => 'getTransitionDuration';
     const getTransitionFunction = () => 'getTransitionFunction';
     
-    const getCurrentPage = () => 'getCurrentPage';
+    const getCurrentPage = () => current;
     
-    const hasDown = () => current < max - 1;
+    const hasDown = () => current < max;
     const hasUp = () => current > 0;
     const hasRight = () => 'hasRight';
     const hasLeft = () => 'hasLeft';
@@ -52,47 +63,89 @@ const ssg = function()
     const scrollDown = () => {
         lock = true;
         setTimeout(() => lock=false, TIMEOUT);
-        setPage(++current);
-        console.log('scrollDown');
+        scrollTo(++current);
     };
+
     const scrollUp = () => {
         lock = true;
         setTimeout(() => lock=false, TIMEOUT);
-        setPage(--current);
-        console.log('scrollUp')
+        scrollTo(--current);
     };
-    const scrollTo = (pageNum) => 'scrollTo' + pageNum;
+
+    const scrollTo = (pageNum) => {
+        document.body.style.transform = `translateY(-${unit.vh() * 100 * pageNum}px)`;
+    };
 
     const revealRight = () => 'scrollRight';
     const revealLeft = () => 'scrollLeft';
 
-    const wheelHandler = (event) => {
-        event.stopImmediatePropagation();
+    const handleWheel = (event) => {
+        // Todo: handle special scroll types like mac etc
         if (lock) return;
         if (event.deltaY > 0 && hasDown()) 
             scrollDown();
         else if (event.deltaY < 0 && hasUp())
             scrollUp();
+    };
+
+    const handleKey = (event) => {
+        if (lock) return;
+        let key = event.which;
+        if (UP_KEYS.includes(key) && hasUp())
+            scrollUp();
+        else if (DOWN_KEYS.includes(key) && hasDown())
+            scrollDown();
+    };
+
+    const handleTouch = (event) => {
+        let startY = event.touches[0].screenY;
+        
+        function handleSwipe(evt) {
+            if (lock) return;
+            let deltaY = evt.touches[0].screenY - startY;
+            if (deltaY > TOUCH_SENSITIVITY && hasUp())
+                scrollUp();
+            else if (deltaY < -TOUCH_SENSITIVITY && hasDown())
+                scrollDown();
+
+            document.removeEventListener('touchmove', handleSwipe);
+        };
+
+        document.addEventListener('touchmove', handleSwipe);
+    }
+
+    const computeCSS = () => `
+        html, body {
+            margin: 0!important;
+            padding: 0!important;
+            overflow: hidden!important;
+        }
+        .ssg.page {
+            box-sizing: border-box!important;
+            height: ${100 * unit.vh()}px!important;
+        }
+    `;
+
+    const handleResize = (event) => {
+        select('#ssgStyle').innerHTML = computeCSS();
+        setPage(current);
     }
 
     const init = () => {
-        var css = `
-            html, body {
-                margin: 0;
-                padding: 0;
-                overflow: hidden;
-            }
-            .ssg.page {
-                height: ${100 * unit.vh()}px;
-            }
-        `
+        var css = computeCSS();
         let style = create('style');
         style.innerHTML = css;
+        style.setAttribute('id', 'ssgStyle');
         document.body.appendChild(style);
 
-        max = selectAll('.ssg.page').length;
+        max = selectAll('.ssg.page').length-1;
 
-        document.addEventListener('wheel', wheelHandler);
+        window.addEventListener('resize', handleResize);
+        document.addEventListener('wheel', handleWheel);
+        document.addEventListener('keyup', handleKey);
+        document.addEventListener('touchstart', handleTouch);
+
+        setPage(current);
     };
     
     window.onload = init;
